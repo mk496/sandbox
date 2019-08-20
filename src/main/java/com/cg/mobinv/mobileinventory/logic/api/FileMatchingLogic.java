e3a5c466052b8eadd4e9dcd8701b7756f2fd7c7a
mk496 <mk496@interia.pl>
2019-08-20 18:08:35 +0200

    ok2

--- src/main/java/com/cg/mobinv/mobileinventory/logic/api/FileMatchingLogic.java
+++
@@ -1,6 +0,0 @@
-package com.cg.mobinv.mobileinventory.logic.api;
-
-import com.cg.mobinv.mobileinventory.common.api.to.FileMatchingTo;
-
-public interface FileMatchingLogic extends LogicComponent<FileMatchingTo> {
-}
